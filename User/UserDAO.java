a6536719221583160096d0a4f2df262f235404b3
ZachH155 <zachary.hulan@keyin.com>
2024-08-11 22:06:48 -0230

    I don't know what to write here

--- User/UserDAO.java
+++ User/UserDAO.java
@@ -1,14 +1,28 @@
 package User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import DbConnection.DbConnection;
+
 public class UserDAO {
 
     public void addUser(User user) {
         String sql = "INSERT INTO accounts (username, userpass, useremail, userrole) VALUES (?, ?, ?, ?)";
 
-        try {
+        try (Connection postgresConnection = DbConnection.getConnection();){
             
+            PreparedStatement pStatement = postgresConnection.prepareStatement(sql);
+
+            pStatement.setString(1, user.getUsername());
+            pStatement.setString(2, user.getPassword());
+            pStatement.setString(3, user.getEmail());
+            pStatement.setString(4, user.getRole());
+
+            pStatement.executeUpdate()  ;
+
         } catch (Exception e) {
-            // TODO: handle exception
+            System.err.println("Connection to database failed");
         }
     }
 }
